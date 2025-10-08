0dd73710043187b14fe23fbaa816d3ee5ea8bdab
leonid <penguin.ognev@gmail.com>
2025-10-09 01:01:39 +0700

    fix: Delite inline in methods

--- src/include/list/ListBase.h
+++ src/include/list/ListBase.h
@@ -30,30 +30,30 @@ class ListBase {
     reference operator*() const { return current->data; }
     pointer operator->() const { return &current->data; }
 
-    inline iterator& operator++() {
+    iterator& operator++() {
       current = current->next;
       return *this;
     }
-    inline iterator operator++(int) {
+    iterator operator++(int) {
       iterator tmp = *this;
       current = current->next;
       return tmp;
     };
 
-    inline iterator& operator--() {
+    iterator& operator--() {
       current = current->prev;
       return *this;
     }
-    inline iterator operator--(int) {
+    iterator operator--(int) {
       iterator tmp = *this;
       current = current->prev;
       return tmp;
     }
 
-    inline bool operator==(const iterator& other) const {
+    bool operator==(const iterator& other) const {
       return current == other.current;
     }
-    inline bool operator!=(const iterator& other) const {
+    bool operator!=(const iterator& other) const {
       return current != other.current;
     }
   };
@@ -86,14 +86,14 @@ class ListBase {
   void popFront();
   void popBack();
 
-  inline iterator begin() noexcept { return iterator(dummy.next); }
-  inline iterator end() noexcept { return iterator(dummy); }
-  inline iterator rbegin() noexcept { return iterator(dummy.prev); }
-  inline iterator rend() noexcept { return iterator(dummy); }
+  iterator begin() noexcept { return iterator(dummy.next); }
+  iterator end() noexcept { return iterator(dummy); }
+  iterator rbegin() noexcept { return iterator(dummy.prev); }
+  iterator rend() noexcept { return iterator(dummy); }
 
-  inline std::size_t size() const noexcept { return _size; };
+  std::size_t size() const noexcept { return _size; };
   void clear() noexcept;
-  inline bool empty() const noexcept { return _size == 0; };
+  bool empty() const noexcept { return _size == 0; };
 
   ListBase& operator=(const ListBase& other);
   ListBase& operator=(ListBase&& other) noexcept;
